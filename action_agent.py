7a85e758a8fef72fef8bbee9ff345c79512ccd6c
drcdev-gh <631758+drcdev-gh@users.noreply.github.com>
2023-08-08 08:49:11 +0000

    scraping

--- action_agent.py
+++ action_agent.py
@@ -5,6 +5,10 @@ from langchain.callbacks import StdOutCallbackHandler
 from enum import Enum
 import uuid
 import json
+import sqlite3
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
 class UserProfile:
     def __init__(self, gender, age_from, age_to, location, interest):
@@ -49,11 +53,57 @@ class Scraper:
     def __init__(self, scraper_name):
         self.scraper_name = scraper_name
 
+        # Connect to the database (it will be created if not exist)
+        conn = sqlite3.connect('webpages.db')
+        c = conn.cursor()
+
+        # Create a table to store cached webpages
+        c.execute('''
+            CREATE TABLE IF NOT EXISTS webpages (
+                url TEXT PRIMARY KEY,
+                content BLOB
+            )
+        ''')
+
+        conn.commit()
+        conn.close()
+
     def get_initial_actions(self, goal):
         return []
 
     def scrape_page_into_possible_actions(self, page):
         return []
+
+    def scrape_and_cache(self, url):
+        # Check if the URL is already cached
+        conn = sqlite3.connect('webpages.db')
+        c = conn.cursor()
+        c.execute('SELECT content FROM webpages WHERE url=?', (url,))
+        cached_data = c.fetchone()
+        conn.close()
+
+        if cached_data:
+            return cached_data[0]
+
+        # If not cached, scrape the webpage
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
+        }
+        req      = Request(url, headers=headers)
+        response = urlopen(req)
+        if response:
+            response_content = response.read()
+
+            # Cache the scraped webpage
+            conn = sqlite3.connect('webpages.db')
+            c = conn.cursor()
+            c.execute('INSERT INTO webpages (url, content) VALUES (?, ?)', (url, response_content))
+            conn.commit()
+            conn.close()
+
+            return response_content
+
+        return None
 
 class AmazonScraper(Scraper):
     def __init__(self):
@@ -63,22 +113,78 @@ class AmazonScraper(Scraper):
         return [Action(ActionType.QUERY_GOAL, goal, self.generate_amazon_search_url(goal))]
 
     def determine_page_type(self, page):
-        return PageType.SEARCH_RESULTS
-        #return PageType.PRODUCT_DETAILS
+        if page.startswith("https://www.amazon.com/s?k"):
+            return PageType.SEARCH_RESULTS
+        return PageType.PRODUCT_DETAILS
 
     def scrape_page_into_possible_actions(self, page):
         actions = []
         page_type = self.determine_page_type(page)
         if page_type is PageType.SEARCH_RESULTS:
-            actions.append(self.extract_products_from_search_page(page))
+            actions.extend(self.extract_products_from_search_page(page))
         elif page_type is PageType.PRODUCT_DETAILS:
-            actions.append(self.extract_recommendations_from_product_details(page))
-            actions.append(self.extract_checkout_from_product_details(page))
+            actions.extend(self.extract_recommendations_from_product_details(page))
+            actions.extend(self.extract_checkout_from_product_details(page))
 
         return actions
 
     def extract_products_from_search_page(self, page):
-        return []
+        products = []
+        content  = self.scrape_and_cache(page)
+
+        soup = BeautifulSoup(content, "lxml")
+        search_results = soup.find_all(attrs={"data-component-type": "s-search-result"})
+        for index, result in enumerate(search_results, start=1):
+            # Extract HREF URL
+            href_element = result.find("a", class_="a-link-normal")
+            if href_element:
+                href_url = href_element['href']
+                full_url = "https://amazon.com" + href_url
+            else:
+                full_url = None
+
+            # Extract escaped text (title)
+            title_element = result.find("span", class_="a-size-base-plus")
+            escaped_text = title_element.get_text() if title_element else None
+
+            # Extract price
+            price_element = result.find("span", class_="a-offscreen")
+            price = price_element.get_text() if price_element else None
+
+            # Extract list price
+            list_price_element = result.find("span", class_="a-price a-text-price")
+            list_price = list_price_element.find("span", class_="a-offscreen").get_text() if list_price_element else None
+
+            # Extract bestseller status
+            bestseller_element = result.find("span", class_="a-badge-label")
+            bestseller_status = bestseller_element.get_text() if bestseller_element else "Not a Bestseller"
+
+            # Extract star rating
+            star_rating_element = result.find("i", class_="a-icon-star-small")
+            star_rating = star_rating_element.find("span", class_="a-icon-alt").get_text() if star_rating_element else None
+
+            # Print or store the extracted information
+            product_description = None
+            if escaped_text is not None and full_url is not None:
+                product_description = self.add_to_str(product_description, "Product Title", escaped_text)
+                product_description = self.add_to_str(product_description, "Price", price)
+                product_description = self.add_to_str(product_description, "Bestseller Status", bestseller_status)
+                product_description = self.add_to_str(product_description, "Star Rating", star_rating)
+                product_description = self.add_to_str(product_description, "List Price", list_price)
+
+                product = Action(ActionType.CLICK_RESULT, product_description, full_url)
+                products.append(product)
+
+        return products  
+    
+    def add_to_str(self, str, key, item):
+        if item is not None:
+            if str is None:
+                str = key + ": " + item
+
+            str += "; " + key + ": " + item
+
+        return str
 
     def extract_recommendations_from_product_details(self, page):
         return []
@@ -88,7 +194,7 @@ class AmazonScraper(Scraper):
 
     def generate_amazon_search_url(self, search_query):
         base_url = "https://www.amazon.com/s"
-        query_params = { 'k': search_query }
+        query_params = {'k': search_query.replace(' ', '+')}  # Replace spaces with '+'
         encoded_params = '&'.join([f"{key}={value}" for key, value in query_params.items()])
         amazon_search_url = f"{base_url}?{encoded_params}"
         return amazon_search_url
@@ -109,16 +215,18 @@ class Agent:
             next_action = self.choose_from_next_actions()
 
             if next_action is not None:
+                #print(next_action.to_json())
                 self.actions_history.append(next_action)
 
             if next_action is not None and next_action.action_type is not ActionType.CHECKOUT:
                 self.next_possible_actions = self.scraper.scrape_page_into_possible_actions(next_action.target_url)
+                #print(Action.array_to_json(self.next_possible_actions))
             else:
                 break
 
     def choose_from_next_actions(self):
         # Temporary helper for testing
-        if len(self.next_possible_actions) == 1:
+        if len(self.next_possible_actions) >= 1:
             return self.next_possible_actions[0]
         else:
             return None
